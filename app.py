c8e23a19a4e0398ad031bef0e0330e94408a8593
sos1305 <111685203+sos1305@users.noreply.github.com>
2024-08-13 19:35:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -278,10 +278,6 @@ with tab5:
 # Footer section
 st.markdown("<hr class='footer'>", unsafe_allow_html=True)
 
-# About Us
-st.markdown("<h3>About Us</h3>", unsafe_allow_html=True)
-st.write(" MedVision Pro is a collective project by <br> ZAINAB <br>KHUSHI KHARI<br>SOUMYA SHUBHAM<br>AARUSH SACHDEVA",unsafe_allow_html=True)
-
 # More Info about Breast Cancer and This Project
 st.markdown("[More Info about this Project](www.google.com)", unsafe_allow_html=True)
 
